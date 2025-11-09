26c4e22affd80af3773d6971d7446647d6761920
Krna07 <Krna260824@gmail.com>
2025-11-09 11:58:09 +0530

    Hii

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-
+app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI)
